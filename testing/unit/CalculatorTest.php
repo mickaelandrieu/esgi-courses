8ad30d601fadcf1cadbf38bc0bae694cfbcad66d
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-10-12 01:44:10 +0200

    Unit tests for Calculator class

---
+++ testing/unit/CalculatorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SolvoLabs;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * PHPUnit tests for Calculator Class.
+ *
+ * @author Mickaël Andrieu <mickael.andrieu@solvolabs.com>
+ */
+class CalculatorTest extends TestCase
+{
+    public function testAddFunctionWorksAsExpected()
+    {
+        $calculator = new Calculator();
+        $calculator->add(2);
+
+        $this->assertSame(2, $calculator->result());
+    }
+
+    public function testMinusFunctionWorksAsExpected()
+    {
+        $calculator = new Calculator(4);
+        $calculator->minus(2);
+
+        $this->assertSame(2, $calculator->result());
+    }
+}
